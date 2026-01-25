e06c40fdc900bb79f1f6b92578391b2fcba61af8
Brian McCollum <bmccollumww@gmail.com>
2026-01-24 22:50:05 -0500

    taillwind working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function HomePage() {
   return (
-    <div>
+    <main>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main content of the home page.</p>
-    </div>
+    </main>
   );
 }
