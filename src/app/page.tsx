ccac7cac76ee8e04aee6bced3bdb33b308bc6c34
Brian McCollum <bmccollumww@gmail.com>
2026-01-25 11:38:11 -0500

    put menu in layout - not needed in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@
 export default function HomePage() {
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main content of the home page.</p>
+      
     </main>
   );
 }
