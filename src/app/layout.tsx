30e17498acf1effaf9aff38135ad6f0f71297521
Brian McCollum <bmccollumw@gmail.com>
2026-03-18 11:59:36 -0400

    browser icon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import PageTransition from '@/components/PageTransition'
 
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
